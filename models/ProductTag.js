e7a1219d391c0efb34762177b8a255c1d563faf4
Javier <elchinatalpf@github.com>
2023-09-08 18:46:35 -0500

    added tag model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,20 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+// model refers to modelName in Tag.js
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
