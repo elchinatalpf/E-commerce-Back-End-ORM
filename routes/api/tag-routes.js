6d5bae8812fbcaa75aa83b54e7cfb04f01771abb
Javier <elchinatalpf@github.com>
2023-09-09 23:28:30 -0500

    Debbuged get by id in tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -18,7 +18,10 @@ router.get('/', async (req, res) => {
 router.get("/:id", async (req, res) => {
   // find a single tag by its `id`, be sure to include its associated Product data
   try {
-    const tagData = await Tag.findByPk(req.params.id, {
+    const tagData = await Tag.findOne({
+      where: {
+        id: req.params.id
+      },
       includes: [{ model: Product, through: ProductTag, as: "producTag_products" } ]
     });
     if (tagData) {
@@ -33,7 +36,9 @@ router.get("/:id", async (req, res) => {
 // create a new tag
 router.post('/', async (req, res) => {
   try {
-    const tagData = await Tag.create(req.body);
+    const tagData = await Tag.create({
+      tag_name: req.body.tag_name
+    });
     res.status(200).json(tagData);
   }catch (err) {
     res.status(500).json(err);
